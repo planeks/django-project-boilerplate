611dc55435f3b512d86ecd9b98427f3b2c66ffc5
Oleh Korkh <olehkorkh@planeks.net>
2020-02-22 14:15:56 +0200

    add accounts migrations and fix urlconf

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,16 +1,1 @@
-from django.http import JsonResponse
-from .settings import LANGUAGE_CHOICES
 
-
-def language_choices_view(request):
-    term = request.GET.get('term', '')
-    data = {
-        'results': [
-            {
-                'id': x[0],
-                'title': x[1],
-            } for x in LANGUAGE_CHOICES if x[1].lower().startswith(term.lower())
-        ],
-        'more': True,
-    }
-    return JsonResponse(data)
--- demo/urls.py
+++ demo/urls.py
@@ -1,21 +1,32 @@
-"""demo URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.views.generic import TemplateView
+
+
+admin.site.site_header = 'Admin console'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', include('apps.core.urls')),
+    # path('select2/', include('django_select2.urls')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
+
+
+# For debug mode only
+if settings.DEBUG:
+    # Turn on debug toolbar
+    import debug_toolbar
+    urlpatterns += [
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
+    # Serve media files via Django
+    import django.views.static
+    urlpatterns += [
+        re_path(r'media/(?P<path>.*)$',
+            django.views.static.serve, {
+                'document_root': settings.MEDIA_ROOT,
+                'show_indexes': True,
+            }),
+    ]
